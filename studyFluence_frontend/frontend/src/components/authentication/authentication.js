dd0fb9b42337d911cccbc9cb591f493abfd2e70e
unknown <n.manodya@gmail.com>
2023-03-15 16:28:12 +0530

    fix format

--- studyFluence_frontend/frontend/src/components/authentication/authentication.js
+++ studyFluence_frontend/frontend/src/components/authentication/authentication.js
@@ -4,40 +4,35 @@ import StudentsView from "./studentsView";
 import ProffesionalView from "./professionalsView";
 
 function Authentication() {
-  const [authenticationType, setAuthenticationType] = useState('std');
+  const [authenticationType, setAuthenticationType] = useState("std");
 
-
-  const onClickProffesionalsViewBtn=()=>{
-	setAuthenticationType('prof')
-  }
-  const onClickStudentsViewBtn=()=>{
-	setAuthenticationType('std')
-  }
+  const onClickProffesionalsViewBtn = () => {
+    setAuthenticationType("prof");
+  };
+  const onClickStudentsViewBtn = () => {
+    setAuthenticationType("std");
+  };
 
   return (
     <div>
       <h1>Welcome to StudyFluence</h1>
 
-	{(authenticationType==='prof')
-        ?  <ProffesionalView authenticationType = {authenticationType}/>
-        : <StudentsView authenticationType ={authenticationType}/>
-      }
-	  <footer>
-
-	  {(authenticationType==='prof')
-        ? 
-		<button onClick={onClickStudentsViewBtn} className="singInAsBtn">
-				
-		<u>Click here to Sign in as a Student</u>
-	  </button>:
-		<button onClick={ onClickProffesionalsViewBtn} className="singInAsBtn">
-				
-		<u>Click here to Sign in as a Proffesional</u>
-	  </button>
-        
-      }
-	    	
-		  </footer>
+      {authenticationType === "prof" ? (
+        <ProffesionalView authenticationType={authenticationType} />
+      ) : (
+        <StudentsView authenticationType={authenticationType} />
+      )}
+      <footer>
+        {authenticationType === "prof" ? (
+          <button onClick={onClickStudentsViewBtn} className="singInAsBtn">
+            <u>Click here to Sign in as a Student</u>
+          </button>
+        ) : (
+          <button onClick={onClickProffesionalsViewBtn} className="singInAsBtn">
+            <u>Click here to Sign in as a Proffesional</u>
+          </button>
+        )}
+      </footer>
     </div>
   );
 }
--- studyFluence_frontend/frontend/src/components/authentication/studentsView.js
+++ studyFluence_frontend/frontend/src/components/authentication/studentsView.js
@@ -1,72 +1,76 @@
-function StudentsView (props){
+function StudentsView(props) {
+  const authenticationType = props.authenticationType;
+  const onClickSignUpActive = () => {
+    const container = document.getElementById("container");
+    container.classList.add("right-panel-active");
+  };
 
-    const authenticationType = props.authenticationType
-    const onClickSignUpActive = () => {
-        const container = document.getElementById("container");
-        container.classList.add("right-panel-active");
-      };
-    
-      const onClickSignInActive = () => {
-        const container = document.getElementById("container");
-        container.classList.remove("right-panel-active");
-      };
+  const onClickSignInActive = () => {
+    const container = document.getElementById("container");
+    container.classList.remove("right-panel-active");
+  };
 
-    return (
-        <div>
-                  
+  return (
+    <div>
       <h2>Students Guide</h2>
-        
-    <div className="container" id="container">
-    <div className="form-container sign-up-container">
-      <form action="#">
-        <h1>Create Account</h1>
 
-        <span>Use your email for registration</span>
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-    <div className="form-container sign-in-container">
-      <form action="#">
-        <h1>Sign in</h1>
-        <span>Enter your login credientials here</span>
-        <input type="username" placeholder="User Name" />
-        <input type="password" placeholder="Password" />
-        <a href="#">Forgot your password?</a>
-        <button>Sign In</button>
-      </form>
-    </div>
-    <div className="overlay-container">
-      <div className="overlay">
-        <div className="overlay-panel overlay-left">
-          <h1>Welcome Back!</h1>
-          <p>
-            To keep connected with us please Sign in with your personal
-            info.
-          </p>
-          <button className="ghost" id="signIn" onClick={onClickSignInActive}>
-            Sign In
-          </button>
+      <div className="container" id="container">
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h1>Create Account</h1>
+
+            <span>Use your email for registration</span>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
         </div>
-        <div className="overlay-panel overlay-right">
-          <h1>Hello,</h1>
-          <p>
-            You are registering with StudentFluence as a Student. Are you
-            professional ?{" "}
-          </p>
-          <button className="ghost" id="signUp" onClick={onClickSignUpActive}>
-            Sign Up
-          </button>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign in</h1>
+            <span>Enter your login credientials here</span>
+            <input type="username" placeholder="User Name" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forgot your password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please Sign in with your personal
+                info.
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={onClickSignInActive}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello,</h1>
+              <p>
+                You are registering with StudentFluence as a Student. Are you
+                professional ?{" "}
+              </p>
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={onClickSignUpActive}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  </div>
-  )
-    
-
+  );
 }
 
-export default StudentsView
+export default StudentsView;
